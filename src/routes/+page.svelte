b3799ff120cc0852f4e4d73af0f045d313fcb8f8
zenitsugocrazy <tuandattobi@gmail.com>
2025-12-13 17:10:38 +0700

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+	let count = 0;
+	let name = 'Svelte';
+
+	$: count = count * 2;
+</script>
+
+<h1>Xin chào {name}!</h1>
+
+<input bind:value={name} placeholder="Đổi tên ở đây" />
+
+<p>
+	Số hiện tại: <strong>{count}</strong> <br />
+</p>
+
+<button on:click={() => (count += 1)}> Tăng số </button>
